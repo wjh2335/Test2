5d5f2a2eea11cdfb3c67ca15546f6443562fbb7d
woo <woo@DESKTOP-GGF9A2H>
2022-12-09 00:25:16 +0900

    초기 커밋.

---
+++ MainWindow.xaml.cpp
@@ -0,0 +1,61 @@
+﻿#include "pch.h"
+#include <microsoft.ui.xaml.window.h>
+#include "MainWindow.xaml.h"
+#if __has_include("MainWindow.g.cpp")
+#include "MainWindow.g.cpp"
+#endif
+
+using namespace winrt;
+using namespace Microsoft::UI::Xaml;
+using namespace Microsoft::UI;
+using namespace Microsoft::UI::Xaml::Media;
+using namespace Microsoft::UI::Xaml::Controls;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace winrt::Test2::implementation
+{
+    MainWindow::MainWindow()
+    {
+        InitializeComponent();
+
+        auto windowNative{ this->try_as<::IWindowNative>() };
+        HWND hWnd{ 0 };
+        windowNative->get_WindowHandle(&hWnd);
+        SetWindowPos(hWnd, NULL, 200, 200, 255, 315, NULL);
+        this->Title(L"ListBox");
+    }
+
+    int32_t MainWindow::MyProperty()
+    {
+        throw hresult_not_implemented();
+    }
+
+    void MainWindow::MyProperty(int32_t /* value */)
+    {
+        throw hresult_not_implemented();
+    }
+
+    void MainWindow::ListBox_SelectionChanged(IInspectable const& /*sender*/, SelectionChangedEventArgs const& /*e*/)
+    {
+        switch (lb1().SelectedIndex())
+        {
+        case 0:
+            Control1Output().Fill(SolidColorBrush(Colors::Blue()));
+            break;
+
+        case 1:
+            Control1Output().Fill(SolidColorBrush(Colors::Green()));
+            break;
+
+        case 2:
+            Control1Output().Fill(SolidColorBrush(Colors::Red()));
+            break;
+
+        case 3:
+            Control1Output().Fill(SolidColorBrush(Colors::Yellow()));
+            break;
+        }
+    }
+}
---
+++ MainWindow.xaml.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+
+#include "MainWindow.g.h"
+
+namespace winrt::Test2::implementation
+{
+    struct MainWindow : MainWindowT<MainWindow>
+    {
+        MainWindow();
+
+        int32_t MyProperty();
+        void MyProperty(int32_t value);
+        void ListBox_SelectionChanged(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::Controls::SelectionChangedEventArgs const& e);
+    };
+}
+
+namespace winrt::Test2::factory_implementation
+{
+    struct MainWindow : MainWindowT<MainWindow, implementation::MainWindow>
+    {
+    };
+}
